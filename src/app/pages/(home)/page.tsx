e8b8105410181e2514ed342539daae59f3023c2a
Rafael da Conceição <113853780+dev-Raffa@users.noreply.github.com>
2023-05-31 18:53:23 +0000

    fixing lint

--- src/app/pages/(home)/page.tsx
+++ src/app/pages/(home)/page.tsx
@@ -1,16 +1,16 @@
-import Image from 'next/image'
-import { SectionFlex } from '@/components/containers/section/index'
-import picture from '../../../../public/imgens/background.png'
+import Image from 'next/image';
+import { SectionFlex } from '@/components/containers/section/index';
+import picture from '../../../../public/imgens/background.png';
 
 export default function Home() {
-    return (
-        <SectionFlex direction='row'>
-            <Image
-            src={picture}
-            alt='enfermeira seurando um globo terrestre'
-            height={500}
-            width={500}
-            />
-        </SectionFlex>
-    )
+  return (
+    <SectionFlex direction="row">
+      <Image
+        src={picture}
+        alt="enfermeira seurando um globo terrestre"
+        height={500}
+        width={500}
+      />
+    </SectionFlex>
+  );
 }
