d397d58a583907ccc8ba7231f633e4f6eec5dc23
Rafael da Conceição <113853780+dev-Raffa@users.noreply.github.com>
2023-05-31 14:50:58 -0300

    create initial page

---
+++ src/app/pages/(home)/page.tsx
@@ -0,0 +1,10 @@
+import SectionFlex from '@/components/containers/section/index.tsx'
+
+
+export default function Home () {
+    return (
+        <SectionFlex >
+            <img src="public/imgens/pexels-anna-shvets-4167542-removebg-preview.png" alt=" enfermeira segurando globo terrestre" />
+        </SectionFlex>
+    )
+}
