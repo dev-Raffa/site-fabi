f7acec13d3b10d53a2329c7f616280e27a2f48c4
Rafael da Conceição <113853780+dev-Raffa@users.noreply.github.com>
2023-05-31 18:33:07 +0000

    initial page

--- src/app/pages/(home)/page.tsx
+++ src/app/pages/(home)/page.tsx
@@ -1,10 +1,16 @@
-import SectionFlex from '@/components/containers/section/index.tsx'
+import Image from 'next/image'
+import { SectionFlex } from '@/components/containers/section/index'
+import picture from '../../../../public/imgens/background.png'
 
-
-export default function Home () {
+export default function Home() {
     return (
-        <SectionFlex >
-            <img src="public/imgens/pexels-anna-shvets-4167542-removebg-preview.png" alt=" enfermeira segurando globo terrestre" />
+        <SectionFlex direction='row'>
+            <Image
+            src={picture}
+            alt='enfermeira seurando um globo terrestre'
+            height={500}
+            width={500}
+            />
         </SectionFlex>
     )
 }
